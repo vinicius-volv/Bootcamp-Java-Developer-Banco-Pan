30005332e3c7eff901957fb8f8578b732d39eefc
Vinicius <vinicius.volv@hotmail.com>
2023-03-07 22:55:41 -0300

    Projeto para exemplo de Classes

---
+++ Trilha-Java-Basico/Anatomia-Classes/src/edu/primeiraSemana/minhaClasse.java
@@ -0,0 +1,19 @@
+package edu.primeiraSemana;
+
+public class minhaClasse {
+
+  public static void main(String[] args) {
+
+    String primeiroNome = "Vinicius";
+    String segundoNome = "Lopes";
+    String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+
+    System.out.println(nomeCompleto);
+
+  }
+
+  public static String nomeCompleto(String primeiroNome, String segundoNome) {
+    return "Resultado do método: " + primeiroNome.concat(" ").concat(segundoNome);
+
+  }
+}
---
+++ Trilha-Java-Basico/Anatomia-Classes/src/edu/segundaSemana/boletimEstudantil.java
@@ -0,0 +1,17 @@
+package edu.segundaSemana;
+
+public class boletimEstudantil {
+  
+  public static void main(String[] args) {
+
+    int mediaFinal = 7;
+    if (mediaFinal < 6)
+
+      System.out.println("REPROVADO");
+    else if (mediaFinal == 6)
+      System.out.println("PROVA MINERVA");
+    else
+      System.out.println("APROVADO");
+
+  }
+}
