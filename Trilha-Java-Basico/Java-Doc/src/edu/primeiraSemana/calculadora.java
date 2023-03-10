40d7996effa39c68d6fd7a353662492576cbb8a2
Vinicius <vinicius.volv@hotmail.com>
2023-03-10 17:30:23 -0300

    Adicionando arquico Java-Doc

---
+++ Trilha-Java-Basico/Java-Doc/src/edu/primeiraSemana/calculadora.java
@@ -0,0 +1,29 @@
+package edu.primeiraSemana;
+/**
+* <h1>Calculadora</h1>
+* A Calculadora realiza operações matemáticas entre números inteiros
+* <p>
+* <b>Note:</b> Leia atentamente a documentaão desta classes
+* para desfrutar dos recursos oferecidos pelo autor
+*
+* @author Gleyson Sampaio
+* @version 1.0
+* @since 01/01//2022
+*/
+public class calculadora {
+  /**
+   * Este método é utilizado para somar dois números inteiros
+   * @param numeroUm este é o primeiro parâmetro do método
+   * @param numeroDois este é o segundo parâmetro do método
+   * @return int o resultado deste método deste método é a soma dos dois números
+   */
+   public int somar(int numeroUm, int numeroDois) {
+    return numeroUm + numeroDois;
+   }
+   /**
+    * No terminal execute o camando abaixo para criar uma
+    * documentação HTML para disponibilizar via web
+    *
+    * javadoc -encoding UTF-8 -docencoding ISO-8859-1  -d ../docs  src/*.java 
+    */
+}
